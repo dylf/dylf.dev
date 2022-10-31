2366f77cc00c8ff018e9bd8c89d4c6e1114575b7
Dylan Fontaine <dylanfontaine@live.ca>
2022-10-30 17:18:00 -0700

    fix: update env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+/** @type {import('astro/config').AstroUserConfig} */
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    define: {
+      'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(
+        process.env.VERCEL_ANALYTICS_ID
+      ),
+    },
+  },
 });
