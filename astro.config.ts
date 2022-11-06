e43871712234de378e3aeea9c926bef121b4b6ac
Dylan Fontaine <dylanfontaine@live.ca>
2022-11-06 15:35:30 -0800

    feat: add astro prefetch

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from "astro-compress";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), prefetch()],
   vite: {
     define: {
       'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
