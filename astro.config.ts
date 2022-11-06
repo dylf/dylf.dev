f43b57e13cc52da022ada58697127a2030bf3a8a
Dylan Fontaine <dylanfontaine@live.ca>
2022-11-06 15:27:36 -0800

    feat: add astro compress

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()],
   vite: {
     define: {
-      'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(
-        process.env.VERCEL_ANALYTICS_ID
-      ),
-    },
-  },
+      'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
+    }
+  }
 });
