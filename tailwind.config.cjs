3477ce6eae27e25b5d1e31856431480ffd907d2f
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-05-17 07:35:16 -0700

    chore: tailwind config formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,35 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: ['Work Sans', ...defaultTheme.fontFamily.sans],
+      sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
     },
     extend: {
       colors: {
-        'custom-purple': '#2a243c',
-        greeny: '#009d52',
-        'active-highlight': '#c78bff',
+        "custom-purple": "#2a243c",
+        greeny: "#009d52",
+        "active-highlight": "#c78bff",
       },
       fontFamily: {
-        headline: ['Signika', ...defaultTheme.fontFamily.sans],
-        subheadline: ['Orienta', ...defaultTheme.fontFamily.sans],
-        code: ['Victor Mono', ...defaultTheme.fontFamily.mono],
+        headline: ["Signika", ...defaultTheme.fontFamily.sans],
+        subheadline: ["Orienta", ...defaultTheme.fontFamily.sans],
+        code: ["Victor Mono", ...defaultTheme.fontFamily.mono],
       },
       animation: {
-        'gradient-xy': 'gradient-xy 10s ease infinite',
+        "gradient-xy": "gradient-xy 10s ease infinite",
       },
       keyframes: {
-        'gradient-xy': {
-          'from, to': {
-            'background-size': '500% 900%',
-            'background-position': 'left bottom',
+        "gradient-xy": {
+          "from, to": {
+            "background-size": "500% 900%",
+            "background-position": "left bottom",
           },
-          '50%': {
-            'background-size': '300% 300%',
-            'background-position': 'right center',
+          "50%": {
+            "background-size": "300% 300%",
+            "background-position": "right center",
           },
         },
       },
