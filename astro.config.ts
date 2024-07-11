a7392e50c6504de4e9490d1a2daab682ed61d7ad
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-07-10 19:28:12 -0700

    chore: migrate to vercel astro adapter

--- astro.config.ts
+++ astro.config.ts
@@ -3,26 +3,24 @@ import tailwind from "@astrojs/tailwind";
 import wasm from "vite-plugin-wasm";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   site: "https://dylf.dev",
   integrations: [wasm(), tailwind(), sitemap()],
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/noop",
-    },
-  },
   prefetch: true,
   vite: {
     optimizeDeps: {
       exclude: ["@dylf/wasm-game-of-life"],
     },
     plugins: [wasm()],
-    define: {
-      "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(
-        process.env.VERCEL_ANALYTICS_ID
-      ),
-    },
   },
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
 
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly VERCEL_ANALYTICS_ID?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
